f946153917eb0de6648a099a643d1c343148005d
Muzhda Ehsan <muzhda.ehsan@dcmail.ca>
2020-12-13 20:04:52 -0500

    Add project files.

---
+++ MidSizeLLP/LendOut.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MidSizeLLP
+{
+    /// <summary>
+    /// Interaction logic for LendOut.xaml
+    /// </summary>
+    public partial class LendOut : UserControl
+    {
+        public LendOut()
+        {
+            InitializeComponent();
+        }
+        private void addLendOut(object sender, RoutedEventArgs e)
+        {
+
+            try
+            {
+                //object Properties = null;
+
+                if (String.IsNullOrEmpty(txtEmployeeID.Text) || String.IsNullOrEmpty(txtName.Text) || String.IsNullOrEmpty(txtDescEquipment.Text) || String.IsNullOrEmpty(txtContactNumber.Text))
+                {
+                    //pop up to show error
+                    MessageBox.Show("You must fill all the fields.");
+                }
+                else
+                {
+                    int empID;
+                    if (!int.TryParse(txtEmployeeID.Text, out empID))
+                    {
+                        //pop up to show error
+                        MessageBox.Show("ID must be numeric.");
+                    }
+                    else
+                    {
+                        string connectString = Properties.Settings.Default.connect_string; //Properties.Settings.Default.connect_string;
+
+                        //string connectString = Properties.Settings.connect_string;
+                        SqlConnection cn = new SqlConnection(connectString);
+                        //opening the connection
+                        cn.Open();
+
+                        //Inserting query
+                        string insertQuery = "INSERT INTO equipment (empID, name, description, phone) VALUES('" + empID + "', '" + txtName.Text + "', '" + txtDescEquipment.Text + "', '" + txtContactNumber.Text + "')";
+                        //creating a new command and passing it the query + the connection.
+                        SqlCommand command = new SqlCommand(insertQuery, cn);
+                        //executing the query. 
+                        command.ExecuteNonQuery();
+                        //closing the connection (good practice).
+                        cn.Close();
+                        //Show a pop up message box if the record was added successfully.
+                        MessageBox.Show("Record was added successfully!");
+                        //Empty everything if the record was added successfully.
+                        txtEmployeeID.Text = string.Empty;
+                        txtName.Text = string.Empty;
+                        txtDescEquipment.Text = string.Empty;
+                        txtContactNumber.Text = string.Empty;
+                    }
+                }
+
+            }
+            //catching any sort of exceptions that might occur.
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ MidSizeLLP/ViewLendOut.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MidSizeLLP
+{
+    /// <summary>
+    /// Interaction logic for ViewLendOut.xaml
+    /// </summary>
+    public partial class ViewLendOut : UserControl
+    {
+        public ViewLendOut()
+        {
+            InitializeComponent();
+            FillDataGrid();
+        }
+
+        private void FillDataGrid()
+        {
+            try
+            {
+                string connectString = Properties.Settings.Default.connect_string; 
+                SqlConnection cn = new SqlConnection(connectString);
+                //opening the connection
+                cn.Open();
+                //This is the SQL query we want to run.
+                string selectionQuery = "SELECT * FROM equipment";
+                //Creating a command and passing the SqlCommand method the query and the connection.
+                SqlCommand command = new SqlCommand(selectionQuery, cn);
+                /*Use an SqlDataAdapter
+                 * A DataAdapter is used to retrieve data from a data source and populate tables within a DataSet
+                 * How do I know this? Read the documentation for SqlClient!  
+                 */
+                SqlDataAdapter sda = new SqlDataAdapter(command);
+                /*Make sure you import "using System.Data;"
+                 * This datatable is being linked with the Cars table.
+                 */
+                DataTable dt = new DataTable("equipment");
+                //sda is the SqlDataAdapter, remember the dataadapter is used to retrieve data from a data source and populate tables within a dataset.
+                sda.Fill(dt);
+
+                /*carsGrid is being bound to the datatable.
+                 *cars grid is a data grid which is just an element in our XAML
+                 * that is being bound and used to display the data from our datatable.
+                 */
+
+                equipmentGrid.ItemsSource = dt.DefaultView;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
+
+
+
+        }
+    }
+}
